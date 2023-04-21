2d996a395c9c8bec2809d4d0e5927ab6d7ac63f8
miminari <mimi@miminari-MBP2021.local>
2023-04-21 10:39:46 +0900

    update files

---
+++ my-tiny-adjustment-kit/inc/Block_Style.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Block Style Customize Class
+ *
+ * The file that defines the customized block style
+ *
+ * @since      0.0.1
+ *
+ * @package    my-tiny-adjustment-kit
+ * @subpackage inc
+ */
+
+namespace MTA_Kit;
+
+/**
+ * MTA_Kit Block Style
+ */
+class Block_Style {
+	/**
+	 * Init
+	 */
+	public static function init() {
+		add_action( 'after_setup_theme', array( get_called_class(), 'block_style_setup' ) );
+	}
+
+	/**
+	 * Add block styles
+	 */
+	public static function block_style_setup() {
+		// wp-block-button.
+		register_block_style(
+			'core/button',
+			array(
+				'name'  => 'arrow',
+				'label' => '矢印付き',
+			)
+		);
+		// wp-block-heading.
+		register_block_style(
+			'core/heading',
+			array(
+				'name'  => 'line',
+				'label' => '線あり',
+			)
+		);
+	}
+}
